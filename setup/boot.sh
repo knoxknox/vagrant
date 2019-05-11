13585e5a092e2a8016a7a54da028cb0b3d02314d
knoxknox <alexandr_y@mail.ru>
2019-05-11 22:48:21 +0300

    Init bash prompt automatically

--- setup/boot.sh
+++ setup/boot.sh
@@ -9,6 +9,7 @@ sh dev/rvm.sh
 mkdir -p ~/.vim/colors
 cp home/.bash_prompt ~/.bash_prompt
 cp home/.bash_aliases ~/.bash_aliases
+echo 'source ~/.bash_prompt' >> ~/.bashrc
 
 mkdir ~/.colout && cp home/colout/colout_log.py ~/.colout/colout_log.py
 wget https://raw.githubusercontent.com/knoxknox/dotfiles/master/home/.vimrc -O ~/.vimrc
